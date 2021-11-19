4cd0a32e9749a80a7955b3629814be9634773c55
Pushkar Kadam <pushkarkadam17@outlook.com>
2021-11-19 19:24:28 +0530

    Read time added. Fixes #5.

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener('scroll', processScroll);
+
+    const paragraphs = document.querySelectorAll("p");
+    const readTimeDiv = document.querySelector("#read-time");
+
+    calculateReadTime(paragraphs, readTimeDiv);
 })
 
 let processScroll = () => {
@@ -11,3 +16,19 @@ let processScroll = () => {
 
     document.querySelector("#progress-bar").style.setProperty('--scrollAmount', scrollPercent);
 }
+
+// Calculate read time
+const calculateReadTime = (paragraphs, readTimeDiv) => {
+    let numberOfWords = 0;
+
+    // Average adult reading speed is around 200 to 250 words per minute
+    const averageWPM = 250;
+
+    paragraphs.forEach(paragraph => {
+        numberOfWords += paragraph.innerHTML.split(" ").length;
+    });
+
+    let readTime = numberOfWords / averageWPM;
+
+    readTimeDiv.innerHTML = Math.round(readTime) + " min read";
+};
